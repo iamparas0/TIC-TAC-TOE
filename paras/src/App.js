3c43da188d9e161f2763942d9d3b0c584904c2ae
Paras Vishwakarma <45733116+iamparas0@users.noreply.github.com>
2023-07-18 17:14:59 +0000

    created tic tac toe app

--- paras/src/App.js
+++ paras/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const initialBoard = Array(9).fill(null);
+
+const App = () => {
+  const [board, setBoard] = useState(initialBoard);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const handleCellClick = (index) => {
+    if (board[index] || winner) return;
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    checkWinner(newBoard, currentPlayer);
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  };
+
+  const checkWinner = (board, player) => {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] === player && board[b] === player && board[c] === player) {
+        setWinner(player);
+        return;
+      }
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setCurrentPlayer('X');
+    setWinner(null);
+  };
+
+  const renderCell = (index) => {
+    const value = board[index];
+    return (
+      <div className="cell" onClick={() => handleCellClick(index)}>
+        {value}
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <div className="board">
+        {board.map((cell, index) => renderCell(index))}
+      </div>
+      {winner && (
+        <div className="winner-message">
+          <p>Player {winner} wins!</p>
+          <button onClick={resetGame}>Restart</button>
+        </div>
+      )}
+      <div className="rules">
+        <h2>Rules</h2>
+        <ul>
+          <li>Two players take turns marking cells in a 3x3 grid.</li>
+          <li>The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.</li>
+          <li>If all cells are filled and no player has three marks in a row, the game is a draw.</li>
+        </ul>
+      </div>
+      <footer className="footer">
+        <p>&copy; 2023 Your Game Name. All rights reserved.</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ paras/src/components/navBar.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import "./navBar.css";
+
+function navBar() {
+    return (
+        <div className="navBar">
+        <h1>Paraskjkjkhlkjlijlijlijlojl'jjklijn</h1>
+        </div>
+    );
+    }
+
+    export default navBar;
