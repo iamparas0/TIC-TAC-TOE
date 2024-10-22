b51809af08824381f68a7790c98e957337b29e5c
skanarul8002 <skanarul8002@gmail.com>
2024-10-23 02:43:35 +0530

    implement chatbot

--- paras/src/App.js
+++ paras/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Game from "./Game"
 import Signup from "./Signup"
 import Login from "./Login"
+import Chatbot from './components/Chatbot/Chatbot';
 function App() {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<Game/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
-        
       </Routes>
     </Router>
+    <Chatbot />
+    </>
   );
 }
 
